4571801e78a1a6db6b1826d9a4ce7d39fe679ccc
Erwan Lemattre <94453013+erwanlem@users.noreply.github.com>
2024-05-22 16:42:45 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erwan/Univ/Stage/eve
-# Build directory: /home/erwan/Univ/Stage/eve/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("examples")
